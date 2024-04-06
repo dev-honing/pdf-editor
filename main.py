32a4c7b5e7d1d9d1fd3851c72f2d469fc21b0363
Ho Nyeong <dev.honing@gmail.com>
2024-04-06 09:41:17 +0900

    Feat: inquirer 패키지를 이용해 파일을 터미널에서 쉽게 선택할 수 있게 개선

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from PyPDF2 import PdfReader, PdfWriter
+import os
+import inquirer
 
 # 홀수 페이지만 추출하여 저장하는 함수
 def extract_odd_pages(input_pdf_path, output_pdf_path):
@@ -8,14 +10,24 @@ def extract_odd_pages(input_pdf_path, output_pdf_path):
         reader = PdfReader(file)
         writer = PdfWriter()
 
-        num_pages = len(reader.pages)
-        for page_num in range(0, num_pages, 2):  # 0부터 시작하여 홀수 페이지만 추출
+        num_pages = len(reader.pages)  # PDF 파일의 총 페이지 수
+        for page_num in range(0, num_pages, 2):  # 홀수 페이지만 추출
             writer.add_page(reader.pages[page_num])
 
         with open(output_pdf_path, 'wb') as output_file:
             writer.write(output_file)
 
 if __name__ == "__main__":
-    input_pdf_path = "S25C-0i24032117380.pdf"  # fixme: 추출할 pdf 파일 경로
-    output_pdf_path = "./extracted_odd.pdf"  # fixme: 저장할 pdf 파일 경로
+    files = os.listdir('./Input/')
+    questions = [
+        inquirer.List('input_pdf_file',
+                      message="추출할 PDF 파일을 선택하세요.",
+                      choices=files),
+        inquirer.Text('output_pdf_file', message="추출된 PDF 파일의 이름을 입력하세요.")
+    ]
+    answers = inquirer.prompt(questions)
+
+    input_pdf_path = "./Input/" + answers['input_pdf_file']
+    output_pdf_path = "./Output/" + answers['output_pdf_file']
+
     extract_odd_pages(input_pdf_path, output_pdf_path)
